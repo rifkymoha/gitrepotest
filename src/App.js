f5d721e32cc39e1de8ca80152a570b8facc76910
Rifky <mohamedrifky534@gmail.com>
2023-08-20 11:26:08 +0530

    Adding line 2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
     <h1> Testing Line 1 from master branch</h1>
+
+    <h2> Testing Line 2 try to merge from master to branch1</h2>
     </div>
   );
 }
